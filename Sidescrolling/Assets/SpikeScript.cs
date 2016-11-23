7054c36e7690135537b87416e2e3a7a97d1523b2
DaniDG <DanielleAnne.DeGuzman@georgebrown.ca>
2016-11-22 20:56:33 -0500

    Version 1
    Version 1 of Platformer. Testing health and animations.

---
+++ Sidescrolling/Assets/SpikeScript.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpikeScript : MonoBehaviour {
+
+	public int damage;
+
+	void OnTriggerEnter2D(Collider2D other){
+		if(other.tag == "Player"){
+			other.GetComponent<PlayerController2D>().TakeDamage(damage);
+		}
+	}
+}
